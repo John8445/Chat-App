4fd68d2a73378d5aa17c76ee6c43db87064fb3bc
Mykel-M <mykel.b.mills@gmail.com>
2023-10-03 20:37:27 -0400

    Sign up page branch

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import SignupPage from '../Compnents/signUpPage';
+import SignupPage from './Compnents/signUpPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
