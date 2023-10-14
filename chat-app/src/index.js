93de9e5642baab1c9aba51ebf26a7147fdb8cefe
John8445 <halljohn3366@gmail.com>
2023-10-14 13:48:11 -0400

    Resolved merge conflicts

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,22 +1,14 @@
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
 import React from 'react'
 import ReactDOM  from 'react-dom'
 import LoginPage from './Components/loginPage'
 import SignUpPage from './Components/signUpPagePage'
-=======
-import SignupPage from './Compnents/signUpPage';
->>>>>>> 8ce2b8fe04a85366ce8e34df107e80d260332c05
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <LoginPage />
     <SignUpPage />
-=======
-    <SignupPage />
->>>>>>> 8ce2b8fe04a85366ce8e34df107e80d260332c05
   </React.StrictMode>
 );
 
