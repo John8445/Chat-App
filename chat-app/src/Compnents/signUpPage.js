b3fca6edfa634d3e7781cb91b2e26303a5252113
Mykel-M <mykel.b.mills@gmail.com>
2023-10-14 12:40:58 -0400

    Sign up page complete

--- chat-app/src/Compnents/signUpPage.js
+++ chat-app/src/Compnents/signUpPage.js
@@ -3,41 +3,124 @@ import { useEffect } from 'react';
 import { Fragment } from 'react';
 
 
+
+function ErrorBox({props}){
+    const [errorStates, setErrorStates] = useState({...props});
+    useEffect(() => {
+        setErrorStates({...props})
+    }, [props])
+
+    return (
+    <>
+        <div className='Signup-error-box'>
+            <ul>
+                <li><p className={errorStates.emailError ? "Signup-red-text" : "Signup-black-text"}>Email must be of valid syntax</p></li>
+                <li><p className={errorStates.userNameError ? "Signup-red-text" : "Signup-black-text"}>Username must be three letters or greater</p></li>
+                <li><p className={errorStates.passwordError ? "Signup-red-text" : "Signup-black-text"}>Password must be eight letters or greater</p></li>
+                <li><p className={errorStates.verifyPassError ? "Signup-red-text" : "Signup-black-text"}>Passwords must match</p></li>
+            </ul>
+        </div>
+    </>
+    )
+}
+
 function SignupPage (){
     const [email, setEmail] = useState('');
     const [userName, setUserName] = useState('');
-    const [password, setPassWord] = useState('');
+    const [password, setPassword] = useState('');
     const [verifyPass, setVerifyPass] = useState('');
     const [showError, setShowError] = useState({
-        emailError: false,
+        emailError: true,
         emailErrorMsg: 'Invalid Format',
-        passwordError:false,
+        passwordError:true,
         passwordErrorMsg: 'Min Length: 8',
-        userNameError:false,
+        userNameError:true,
         userNameErrorMsg: 'Min Lenght: 3',
-        verifyPassError: false,
+        verifyPassError: true,
         verifyPassErrorMsf: "Does not match password"
     });
 
+    function validateEmail(e){
+        var re = /\S+@\S+\.\S+/;
+        if(e.target.value == ''){
+            setShowError({...showError,emailError:false});
+        }
+        else if(re.test(e.target.value) == true){
+            setShowError({...showError,emailError:false});
+        }
+        else{
+            setShowError({...showError,emailError:true,emailErrorMsg: 'Invalid Format'});
+        }
+
+        setEmail(e.target.value);
+    }
+
+    function validateUserName(e){
+        let val = e.target.value;
+        if(val == ''){
+            setShowError({...showError,userNameError:false});
+        }
+        else if(val.length < 3){
+            setShowError({...showError,userNameError:true,userNameErrorMsg: 'Min Lenght: 3'});
+        }
+        else{
+            setShowError({...showError,userNameError:false});
+        }
+        setUserName(val);
+    }
+
+    function validatePassword(e){
+        let val = e.target.value;
+        if(val == ''){
+            setShowError({...showError,passwordError:true});
+        }
+        else if(val.length < 8){
+            setShowError({...showError,passwordError:true, passwordErrorMsg: 'Min Length: 8'});
+        }
+        else {
+            setShowError({...showError,passwordError:false});
+        }
+        setPassword(val);
+    }
+
+    function validateVerifyPassword(e){
+        let val = e.target.value;
+        if(val == ''){
+            setShowError({...showError,verifyPassError:false});
+        }
+        else if(val !== password){
+            setShowError({...showError,verifyPassError:true});
+        }
+        else if(val == password){
+            setShowError({...showError,verifyPassError:false});
+        }
+        setVerifyPass(val);
+    }
 
     return (
     <>
         <div className="Signup-main-container">
+            <ErrorBox props={{...showError}}></ErrorBox>
             <div className='Signup-signup-container'>
                 <div className='Signup-signup-main-content'> 
                     <p className='Signup-signup-title'>QwickChat</p> 
                     <div className='Signup-input-field-wrapper'>
-                        <input type='text'></input>
+                        <input type='text' className='Signup-text-input' required onChange={(e) => {validateEmail(e)}}></input>
+                        <span class="Signup-input-title">Email</span>
                     </div>
                     <div className='Signup-input-field-wrapper'>
-                        <input type='text'></input>
+                        <input type='text' className='Signup-text-input' required onChange={(e) => {validateUserName(e)}}></input>
+                        <span class="Signup-input-title">Username</span>
                     </div>
                     <div className='Signup-input-field-wrapper'>
-                        <input type='text'></input>
+                        <input type='text' className='Signup-text-input' required onChange={(e) => {validatePassword(e)}}></input>
+                        <span class="Signup-input-title">Password</span>
                     </div>
                     <div className='Signup-input-field-wrapper'>
-                        <input type='text'></input>
-                    </div>   
+                        <input type='text' className='Signup-text-input' required onChange={(e) => {validateVerifyPassword(e)}}></input>
+                        <span class="Signup-input-title">Verify Password</span>
+                    </div> 
+                    <input type="button" value={"Sign Up"} id="Signup-button" ></input>  
                 </div>
             </div>
         </div>
