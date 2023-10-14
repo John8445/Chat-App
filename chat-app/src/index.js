681d36003858b3f9ba56af629ed328358e46d589
John8445 <halljohn3366@gmail.com>
2023-10-14 12:48:38 -0400

    trying to merge

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,11 +2,13 @@ import reportWebVitals from './reportWebVitals';
 import React from 'react'
 import ReactDOM  from 'react-dom'
 import LoginPage from './Components/loginPage'
+import SignUpPage from './Components/signUpPagePage'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <LoginPage />
+    <SignUpPage />
   </React.StrictMode>
 );
 
